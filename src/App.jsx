0459370b0b13a610b1689e92c63ac320bf721822
AmrAlaaFCIH <amr.alaa.fcih@gmail.com>
2020-05-10 01:38:57 +0200

    replacing classes with functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import React, { Component } from "react";
+import React, { useContext } from "react";
 import Table1 from "./components/Table1";
 import Table2 from "./components/Table2";
 import { Input, Button } from "reactstrap";
 import { AppContext } from "./contextAPI/appContext";
 
-class App extends Component {
-  static contextType = AppContext;
-
-  render() {
-    const { getRowNumber, rows, getFSum, secondTable } = this.context;
-    return (
-      <div>
-        <Input
-          placeholder="Enter Number Of Rows ..."
-          type="number"
-          name="row"
-          onChange={getRowNumber}
-          value={rows}
-        />
-        {rows ? (
-          <div>
-            <Table1 />
-            <Button outline color="primary" onClick={getFSum}>
-              Get Result
-            </Button>
-          </div>
-        ) : null}
-        {secondTable ? <Table2 /> : null}
-      </div>
-    );
-  }
-}
+const App = () => {
+  const { getRowNumber, rows, getFSum, secondTable } = useContext(AppContext);
+  return (
+    <div>
+      <Input
+        placeholder="Enter Number Of Rows ..."
+        type="number"
+        name="row"
+        onChange={getRowNumber}
+        value={rows}
+      />
+      {rows ? (
+        <div>
+          <Table1 />
+          <Button outline color="primary" onClick={getFSum}>
+            Get Result
+          </Button>
+        </div>
+      ) : null}
+      {secondTable ? <Table2 /> : null}
+    </div>
+  );
+};
 
 export default App;
